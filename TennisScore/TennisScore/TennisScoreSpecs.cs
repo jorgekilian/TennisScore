24184a13309af8f0724581865037e06620caa028
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-04-06 16:50:13 +0100

    player_two_has_0_points_when_tennis_game_start

--- TennisScore/TennisScore/TennisScoreSpecs.cs
+++ TennisScore/TennisScore/TennisScoreSpecs.cs
@@ -15,14 +15,24 @@ namespace TennisScoreSpecs {
 
             Assert.AreEqual("Love", tg.player1.Points);
         }
+
+        [Test]
+        public void player_two_has_0_points_when_tennis_game_start() {
+
+            var tg = new TennisGame();
+            tg.Start();
+
+            Assert.AreEqual("Love", tg.player2.Points);
+        }
     }
 
     public class TennisGame {
-
+        public Player player2 { get; set; }
         public Player player1 { get; set; }
 
         public void Start() {
             player1 = new Player();
+            player2 = new Player();
         }
     }
 
