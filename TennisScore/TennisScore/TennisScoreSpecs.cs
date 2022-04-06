a6d91deb1051d7d54bd9bdc7d7bd97c999c5287e
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-04-06 16:26:48 +0100

    ignore

---
+++ TennisScore/TennisScore/TennisScoreSpecs.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace TennisScoreSpecs {
+    public class Tests {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
